00a0b7bef85f44b422ac0146cfa42194586f9af9
Satis10 <Satishchamrel@gmail.com>
2025-02-27 09:11:22 -0600

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
   });
 });
